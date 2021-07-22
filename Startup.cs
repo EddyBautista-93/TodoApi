55e10b0e61736937b9f67ed51fa426df82382b9d
EddyBautista <bautista.j.eddy@gmail.com>
2021-07-22 18:26:56 -0500

    fixed swagger error from posting to api

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
+using Microsoft.OpenApi.Models;
 
 namespace TodoApi
 {
@@ -24,6 +25,10 @@ namespace TodoApi
             services.AddDbContext<TodoContext>(opt =>
             opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoApi", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
